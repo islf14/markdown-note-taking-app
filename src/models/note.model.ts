ea38f45cbeb0ab186f2085f02524326cd95fc760
Is <isaiaslflores@gmail.com>
2025-07-10 19:26:35 -0500

    format

--- src/models/note.model.ts
+++ src/models/note.model.ts
@@ -6,9 +6,11 @@ async function connect() {
     const client = connection()
     await client.connect()
     const database = client.db('markdown-notes')
-    return database.collection('notes')
-  } catch (e: any) {
-    throw new Error('Database connection failed: ' + e.message)
+    return { client: client, db: database.collection('notes') }
+  } catch (e: unknown) {
+    let message
+    if (e instanceof Error) message = e.message
+    throw new Error('Database connection failed: ' + message)
   }
 }
 
@@ -18,9 +20,9 @@ interface Note {
 }
 
 export class NoteModel {
+  //
   static async saveNote({ name, content }: Note): Promise<object> {
-    const db = await connect()
-
+    const { client, db } = await connect()
     const noteData = {
       name,
       content,
@@ -30,21 +32,27 @@ export class NoteModel {
 
     try {
       const { insertedId } = await db.insertOne(noteData)
+      client.close()
       return insertedId
-    } catch (e: any) {
-      throw new Error('Error saving note: ' + e.message)
+    } catch (e: unknown) {
+      client.close()
+      let message
+      if (e instanceof Error) message = e.message
+      throw new Error('Error saving note: ' + message)
     }
   }
 
   static async gellAll() {
-    const db = await connect()
+    const { client, db } = await connect()
     const allNotes = await db.find().toArray()
+    client.close()
     return allNotes
   }
 
   static async viewNote({ id }: { id: string }) {
-    const db = await connect()
+    const { client, db } = await connect()
     const note = await db.findOne({ _id: new ObjectId(id) })
+    client.close()
     return note
   }
 }
